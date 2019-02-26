e2f39b7bb928db9d8ac63102aeb76bce62573895
Ajinkya Kudoo <ajinkyakudoo@gmail.com>
2019-02-26 16:56:12 +0530

    Intial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MaterialModule} from './material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
